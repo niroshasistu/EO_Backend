26cfb7a7acacf6f72fdb2d29ef95cb01f4f283ae
nirosha <sistunirosha243@gmail.com>
2025-06-14 22:32:05 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// Load .env variables
+dotenv.config();
+console.log('MONGO URI:', process.env.MONGO_URI); // You 
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true // if sending cookies
+}));
+app.use(express.json());
+
+// Routes
+app.use('/api/pickups', require('./routes/pickups'));
+app.use('/api/contact', require('./routes/contact'));
+app.use('/api/impact', require('./routes/impact'));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
